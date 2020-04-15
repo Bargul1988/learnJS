dc7bf977343ca4850fa7f6ac8ba054dacc6db72f
Bargul1988 <48165543+Bargul1988@users.noreply.github.com>
2020-04-15 19:54:29 +0300

    implevent a task large if else

---
+++ Head-First/chapter-00/chelenge-01/larger.js
@@ -0,0 +1,11 @@
+let x=1 ;
+y=1;
+if (x>y){
+    console.log('x больше чем y');
+}else if(x<y){
+    console.log(`${x} меньше чем ${y}`);
+}else if(x==y) {
+    console.log('x равен y');
+}else {
+ console.log('одна из переменных не число');
+}
