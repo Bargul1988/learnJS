7e89f47eba98f4275f7e7c720bd792b2ecd2bf06
Bargul1988 <48165543+Bargul1988@users.noreply.github.com>
2020-04-19 23:41:37 +0300

    add the task a not challenge

---
+++ Head-First/chapter-00/challenge-00-chat/chat.js
@@ -0,0 +1,35 @@
+let button  = document.querySelector('.send');
+console.log(button);
+console.log('hello');
+
+button.value ='Hi';
+
+
+let textareaSend = () => {
+
+document.querySelector('.outputField2').value;
+document.querySelector('.outputField2').value;
+    document.querySelector('.outputField2').value=document.querySelector('.outputField2').value+document.querySelector('.outputField').value+' '
+
+
+
+    console.log(document.querySelector('.outputField2').value)
+    console.log(document.querySelector('.outputField').value)
+ }
+
+let textareaSend2= ()=>{
+    document.querySelector('.outputField2').value =document.querySelector('.outputField2').value+document.querySelector('.outputField3').value + ' '
+}
+
+
+
+
+// const textareainput = () => {
+//
+// }
+
+
+
+
+// textareaSend()
+// console.log(textareaSend())
