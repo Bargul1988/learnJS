2023fbd781dae9c90b342feaab2961718c3a3d40
Bargul1988 <48165543+Bargul1988@users.noreply.github.com>
2020-04-30 19:50:19 +0300

    implementation of the task №1-array reverse

---
+++ Head-First/chapter-04-array/challenge-01/reverse.js
@@ -0,0 +1,13 @@
+
+function reverse(values) {
+    debugger
+    let quantity  = values.reverse()
+     return quantity;
+
+}
+
+let res = reverse([44,6,63,73,75,72,24,7,0]);
+
+console.log(res);
+
+
