ffaa61736c00f7a91a29ab44829e1f3573eda02a
Bargul1988 <48165543+Bargul1988@users.noreply.github.com>
2020-05-07 12:50:55 +0300

    implementation of the task №8-array array to string

---
+++ Head-First/chapter-04-array/challenge-08/array-to-string.js
@@ -0,0 +1,9 @@
+function arrayToString(values) {
+
+    values = values.join('');
+    return values;
+
+}
+
+let result = arrayToString(['my', '88', '@', 'gmail', '.', 'com']);
+console.log(result);
